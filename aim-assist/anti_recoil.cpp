790a88e5711d190b70a6c6b858ae2f4bd6dce755
venomz75 <36103496+venomz75@users.noreply.github.com>
2021-09-27 23:30:01 +0100

    Update anti_recoil.cpp

--- aim-assist/anti_recoil.cpp
+++ aim-assist/anti_recoil.cpp
@@ -1,9 +1,9 @@
 #include "anti_recoil.h"
 
-void anti_recoil::removeScreenshake(uintptr_t ptrLocalPlayer, uintptr_t ptrRecoilPitch, uintptr_t ptrRecoilYaw, uintptr_t ptrRecoilRoll, uintptr_t ptrDrunkness)
+void anti_recoil::removeScreenshake(uintptr_t ptrLocalPlayer, uintptr_t ptrRecoilPitch, uintptr_t ptrRecoilYaw, uintptr_t ptrRecoilRoll, uintptr_t ptrScreenshake)
 {
 	*(float*)(ptrLocalPlayer + ptrRecoilPitch) = 0;
 	*(float*)(ptrLocalPlayer + ptrRecoilYaw) = 0;
 	*(float*)(ptrLocalPlayer + ptrRecoilRoll) = 0;
-	*(float*)(ptrLocalPlayer + ptrDrunkness) = 0;
+	*(float*)(ptrLocalPlayer + ptrScreenshake) = 0;
 }
